7cacd5b72134d1f19d35e0d8c1c7f64113f625c3
Charles Dixon <chvckd@gmail.com>
2023-07-25 11:44:52 +0100

    Add search index management

--- generate.go
+++ generate.go
@@ -11,6 +11,7 @@
 //go:generate protostellar couchbase/admin/bucket/v1/bucket.proto
 //go:generate protostellar couchbase/admin/collection/v1/collection.proto
 //go:generate protostellar couchbase/admin/query/v1/query.proto
+//go:generate protostellar couchbase/admin/search/v1/search.proto
 //go:generate protostellar couchbase/internal/hooks/v1/hooks.proto
 
 package main
